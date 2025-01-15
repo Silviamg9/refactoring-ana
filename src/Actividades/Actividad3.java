e4bef6f83396d83a6fff84100b4187b6db35e5ea
Silvia Mª Moreno García <moreno.garcia.silvia.maria@iescamas.es>
2025-01-15 12:52:53 +0100

    Refactorización de la Actividad3

--- src/Actividades/Actividad3.java
+++ src/Actividades/Actividad3.java
@@ -3,27 +3,25 @@ import java.util.*;
 public class Actividad3 {
 
 	public static void main(String[] args) {
-		var sc = new Scanner (System.in);
-		
-		System.out.println("¿Cuántos números desea introducir?: ");
-		int cantidad = sc.nextInt();
-		
-		int [] numeros = new int[cantidad];
-		
-		System.out.println("Introduzca " + cantidad + " números enteros: ");
-		
-		for(int i = 0; i < cantidad; i++) {
-			System.out.println("Número " + (i+1) + ": ");
-			numeros[i] = sc.nextInt();
-		}
-		
-		Arrays.sort(numeros);
-		
-		
-		System.out.println("Los números introducidos en el orden inverso son: ");
-		for(int i = cantidad -1; i > 0; i--) {
-			System.out.println(numeros[i]);
-		}
-	}
-
+        var sc = new Scanner(System.in);
+        
+        System.out.println("¿Cuántos números desea introducir?: ");
+        int cantidad = sc.nextInt();
+        
+        int[] numeros = new int[cantidad];
+        
+        System.out.println("Introduzca " + cantidad + " números enteros: ");
+        
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println("Número " + (i + 1) + ": ");
+            numeros[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(numeros);
+        
+        System.out.println("Los números introducidos en el orden inverso son: ");
+        for (int i = cantidad - 1; i >= 0; i--) {
+            System.out.println(numeros[i]);
+        }
+    }
 }
